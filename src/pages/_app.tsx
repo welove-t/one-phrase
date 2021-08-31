4f755c1ad7a12a4a3cfd195fd256e4725cc3399a
welove-t <tlove.tky@gmail.com>
2021-09-01 07:12:27 +0900

    💄 toaster設置

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import 'tailwindcss/tailwind.css';
 import Footer from '../components/Footer';
 import UserProvider from '../context/userContext';
+import { Toaster } from 'react-hot-toast';
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <UserProvider>
       <Component {...pageProps} />
       <Footer />
+      <Toaster />
     </UserProvider>
   );
 };
